36d7957688520c5d77f48495b7d143392471a452
Kevin Barnes <38760777+Majekdor@users.noreply.github.com>
2020-12-03 01:08:24 -0500

    Day Three Code

--- src/main/java/dev/majek/adventofcode/DayThree/Day3A.java
+++ src/main/java/dev/majek/adventofcode/DayThree/Day3A.java
@@ -1,12 +1,25 @@
 package dev.majek.adventofcode.DayThree;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Day3A {
 
     /**
      * Code for day three programming challenge A
-     * Answer for my values:
+     * Answer for my values: 268
      */
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        String data = new String(Files.readAllBytes(Paths.get("C:\\Users\\ksbar\\Documents\\GitHub\\" +
+                "AdventOfCode\\src\\main\\resources\\day3inputs.txt")));
+        String[] inputs = data.split("\n");
+        int moveRight = 0, runningTotal = 0;
+        for (String input : inputs) {
+            input = input.repeat(100); // Lmao
+            if (input.charAt(moveRight) == '#')
+                runningTotal++;
+            moveRight += 3;
+        }
+        System.out.println("Answer is: " + runningTotal);
     }
 }
--- src/main/java/dev/majek/adventofcode/DayThree/Day3B.java
+++ src/main/java/dev/majek/adventofcode/DayThree/Day3B.java
@@ -1,12 +1,51 @@
 package dev.majek.adventofcode.DayThree;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Day3B {
 
     /**
      * Code for day three programming challenge B
-     * Answer for my values:
+     * Answer for my values: 3093068400
      */
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        String data = new String(Files.readAllBytes(Paths.get("C:\\Users\\ksbar\\Documents\\GitHub\\" +
+                "AdventOfCode\\src\\main\\resources\\day3inputs.txt")));
+        String[] inputs = data.split("\n");
+        int moveRight = 0;
+        int a = 0, b = 0, c = 0, d = 0;
+        for (String input : inputs) {
+            input = input.repeat(1000);
+            if (input.charAt(moveRight) == '#') {
+                a++;
+            }
+            moveRight += 1;
+        }
+        moveRight = 0;
+        for (String input : inputs) {
+            input = input.repeat(1000);
+            if (input.charAt(moveRight) == '#') {
+                b++;
+            }
+            moveRight += 5;
+        }
+        moveRight = 0;
+        for (String input : inputs) {
+            input = input.repeat(1000);
+            if (input.charAt(moveRight) == '#') {
+                c++; // haha
+            }
+            moveRight += 7;
+        }
+        moveRight = 0;
+        for (int i = 0; i <= 323; i+=2) {
+            inputs[i] = inputs[i].repeat(1000);
+            if (inputs[i].charAt(moveRight) == '#') {
+                d++;
+            }
+            moveRight += 1;
+        }
+        System.out.println("Answer is: " + ((long) a * 268 * b * c * d));
     }
 }

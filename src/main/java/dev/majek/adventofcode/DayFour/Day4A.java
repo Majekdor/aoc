0711b0e2476fdb53620b459b1bd243e180ecbf98
Kevin Barnes <38760777+Majekdor@users.noreply.github.com>
2020-12-04 12:41:08 -0500

    Day Four Code

---
+++ src/main/java/dev/majek/adventofcode/DayFour/Day4A.java
@@ -0,0 +1,23 @@
+package dev.majek.adventofcode.DayFour;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Day4A {
+
+    /**
+     * Code for day four programming challenge A
+     * Answer for my values: 264
+     */
+    public static void main(String[] args) throws Exception {
+        String data = new String(Files.readAllBytes(Paths.get("C:\\Users\\ksbar\\Documents\\GitHub\\" +
+                "AdventOfCode\\src\\main\\resources\\day4inputs.txt")));
+        String[] inputs = data.split("\n\n");
+        int counter = 0;
+        for (String input : inputs)
+            if (input.contains("ecl") && input.contains("pid") && input.contains("eyr") && input.contains("hcl") &&
+                    input.contains("byr") && input.contains("iyr") && input.contains("hgt"))
+                counter++;
+        System.out.println(counter);
+    }
+}

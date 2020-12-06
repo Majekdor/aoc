4d72495eca7dfb1df15446dcf0c6bd6b73977c62
Majekdor <ksbarnes39@gmail.com>
2020-12-05 21:57:25 -0500

    Day Five Code+

--- src/main/java/dev/majek/adventofcode/DayFive/Day5A.java
+++ src/main/java/dev/majek/adventofcode/DayFive/Day5A.java
@@ -2,6 +2,9 @@ package dev.majek.adventofcode.DayFive;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Day5A {
 
@@ -12,36 +15,13 @@ public class Day5A {
     public static void main(String[] args) throws Exception {
         String data = new String(Files.readAllBytes(Paths.get("C:\\Users\\ksbar\\IdeaProjects\\" +
                 "AdventOfCode\\src\\main\\resources\\day5inputs.txt")));
-        String[] inputs = data.split("\n");
-        int highestPID = 0;
-        for (String input : inputs) {
-            int frontRow = 0; int backRow = 127; double middleRow = Math.ceil((backRow + frontRow) / 2.0);
-            int leftColumn = 0; int rightColumn = 7; double middleColumn = Math.ceil((leftColumn + rightColumn) / 2.0);
-            char[] chars = input.toCharArray();
-            for (char character : chars) {
-                switch (character) {
-                    case 'F':
-                        backRow = (int) middleRow;
-                        middleRow = Math.ceil((backRow + frontRow) / 2.0);
-                        break;
-                    case 'B':
-                        frontRow = (int) middleRow;
-                        middleRow = Math.ceil((backRow + frontRow) / 2.0);
-                        break;
-                    case 'L':
-                        rightColumn = (int) middleColumn;
-                        middleColumn = Math.ceil((leftColumn + rightColumn) / 2.0);
-                        break;
-                    case 'R':
-                        leftColumn = (int) middleColumn;
-                        middleColumn = Math.ceil((leftColumn + rightColumn) / 2.0);
-                        break;
-                }
-            }
-            int row = (int) Math.min(frontRow, Math.min(middleRow, backRow));
-            int column = (int) Math.min(leftColumn, Math.min(middleColumn, rightColumn));
-            highestPID = Math.max(((row * 8) + column), highestPID);
-        }
-        System.out.println("Answer is: " + highestPID);
+        List<String> inputs = new ArrayList<>(Arrays.asList(data.split("\n")));
+        int[] seatIDs = inputs.stream().mapToInt(Day5A::getSeatID).sorted().toArray();
+        System.out.println("Answer is: " + (seatIDs[seatIDs.length-1]));
+    }
+
+    public static int getSeatID(String input) {
+        return Integer.parseInt(input.replace("F", "0").replace("B", "1")
+                .replace("L", "0").replace("R", "1"), 2);
     }
 }
--- src/main/java/dev/majek/adventofcode/DayFive/Day5B.java
+++ src/main/java/dev/majek/adventofcode/DayFive/Day5B.java
@@ -2,7 +2,9 @@ package dev.majek.adventofcode.DayFive;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Day5B {
 
@@ -13,50 +15,15 @@ public class Day5B {
     public static void main(String[] args) throws Exception {
         String data = new String(Files.readAllBytes(Paths.get("C:\\Users\\ksbar\\IdeaProjects\\" +
                 "AdventOfCode\\src\\main\\resources\\day5inputs.txt")));
-        String[] inputs = data.split("\n");
-        int[] seatIDs = new int[961];
-        for (int i = 0; i < inputs.length; i++) {
-            int frontRow = 0; int backRow = 127; double middleRow = Math.ceil((backRow + frontRow) / 2.0);
-            int leftColumn = 0; int rightColumn = 7; double middleColumn = Math.ceil((leftColumn + rightColumn) / 2.0);
-            char[] chars = inputs[i].toCharArray();
-            for (char c : chars) {
-                switch (c) {
-                    case 'F':
-                        backRow = (int) middleRow;
-                        middleRow = Math.ceil((backRow + frontRow) / 2.0);
-                        break;
-                    case 'B':
-                        frontRow = (int) middleRow;
-                        middleRow = Math.ceil((backRow + frontRow) / 2.0);
-                        break;
-                    case 'L':
-                        rightColumn = (int) middleColumn;
-                        middleColumn = Math.ceil((leftColumn + rightColumn) / 2.0);
-                        break;
-                    case 'R':
-                        leftColumn = (int) middleColumn;
-                        middleColumn = Math.ceil((leftColumn + rightColumn) / 2.0);
-                        break;
-                }
-            }
-            int row = (int) Math.min(frontRow, Math.min(middleRow, backRow));
-            int column = (int) Math.min(leftColumn, Math.min(middleColumn, rightColumn));
-            seatIDs[i] = ((row * 8) + column);
-        }
-        Arrays.sort(seatIDs); int counter = seatIDs[1];
-        for (int i = 1; i < seatIDs.length; i++) {
-            try {
-                if (seatIDs[i] + 1 != seatIDs[i + 1] || seatIDs[i] - 1 == seatIDs[i - 1]) {
-                    counter++; continue;
-                }
-            } catch (Exception ex) {
-                counter++; continue;
-            }
-            if (seatIDs[i] != counter) {
-                System.out.println("Answer is: " + counter); break;
-            }
-            else
-                counter++;
-        }
+        List<String> inputs = new ArrayList<>(Arrays.asList(data.split("\n")));
+        int[] seatIDs = inputs.stream().mapToInt(Day5B::getSeatID).sorted().toArray();
+        for (int i = 1; i < seatIDs.length; i++)
+            if (seatIDs[i] - seatIDs[i-1] != 1)
+                System.out.println("Answer is: " + (seatIDs[i]-1));
+    }
+
+    public static int getSeatID(String input) {
+        return Integer.parseInt(input.replace("F", "0").replace("B", "1")
+                .replace("L", "0").replace("R", "1"), 2);
     }
 }

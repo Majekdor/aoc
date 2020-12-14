bab5937312ed4b2aaf497d008756e4c2ec985aaf
Kevin Barnes <38760777+Majekdor@users.noreply.github.com>
2020-12-14 16:59:56 -0500

    Day Fourteen Code

---
+++ src/main/java/dev/majek/adventofcode/DayFourteen/Day14A.java
@@ -0,0 +1,53 @@
+package dev.majek.adventofcode.DayFourteen;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Day14A {
+
+    /**
+     * Code for day fourteen programming challenge A
+     * Answer for my values: 5875750429995
+     */
+    public static void main(String[] args) throws Exception {
+        String data = new String(Files.readAllBytes(Paths.get("C:\\Users\\ksbar\\Documents\\GitHub\\" +
+                "AdventOfCode\\src\\main\\resources\\day14inputs.txt")));
+        List<String> inputs = Arrays.stream(data.split("\n")).collect(Collectors.toList());
+        String mask = "";
+        Map<Integer, Long> memory = new HashMap<>();
+        for (String input : inputs) {
+            String afterEquals = input.substring(input.indexOf("=") + 2);
+            if (input.contains("mask")) {
+                mask = afterEquals;
+                continue;
+            }
+            int address = Integer.parseInt(input.substring(input.indexOf("[")+1, input.indexOf("]")));
+            StringBuilder value = new StringBuilder(Integer.toString(Integer.parseInt(afterEquals), 2));
+            while (value.length() != 36)
+                value.insert(0, "0");
+            StringBuilder newValue = new StringBuilder(36);
+            for (int i = 0; i < 36; i++) {
+                char[] maskBits = mask.toCharArray();
+                if (maskBits[i] == '0')
+                    newValue.append("0");
+                else if (maskBits[i] == '1')
+                    newValue.append("1");
+                else
+                    newValue.append(value.toString().toCharArray()[i]);
+            }
+            if (memory.containsKey(address))
+                memory.replace(address, Long.parseLong(String.valueOf(newValue), 2));
+            else
+                memory.put(address, Long.parseLong(String.valueOf(newValue), 2));
+        }
+        long answer = 0L;
+        for (long value : memory.values())
+            answer += value;
+        System.out.println("Answer is: " + answer);
+    }
+}
---
+++ src/main/java/dev/majek/adventofcode/DayFourteen/Day14B.java
@@ -0,0 +1,66 @@
+package dev.majek.adventofcode.DayFourteen;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Day14B {
+
+    /**
+     * Code for day fourteen programming challenge B
+     * Answer for my values: 5272149590143
+     */
+    public static void main(String[] args) throws Exception {
+        String data = new String(Files.readAllBytes(Paths.get("C:\\Users\\ksbar\\Documents\\GitHub\\" +
+                "AdventOfCode\\src\\main\\resources\\day14inputs.txt")));
+        List<String> inputs = Arrays.stream(data.split("\n")).collect(Collectors.toList());
+        String mask = "";
+        Map<String, Long> memory = new HashMap<>();
+        for (String input : inputs) {
+            String afterEquals = input.substring(input.indexOf("=") + 2);
+            if (input.startsWith("mask")) {
+                mask = afterEquals;
+                continue;
+            }
+            int address = Integer.parseInt(input.substring(input.indexOf("[")+1, input.indexOf("]")));
+            long value = Long.parseLong(afterEquals);
+            ArrayList<String> addresses = getAddresses(address, mask);
+            for (String add : addresses)
+                if (memory.containsKey(add))
+                    memory.replace(add, value);
+                else
+                    memory.put(add, value);
+        }
+        long answer = 0L;
+        for (long val : memory.values())
+            answer += val;
+        System.out.println("Answer is: " + answer);
+    }
+
+    private static ArrayList<String> getAddresses(int address, String mask) {
+        ArrayList<String> toReturn = new ArrayList<>();
+        toReturn.add("");
+
+        StringBuilder value = new StringBuilder(Integer.toBinaryString(address));
+        while (value.length() < 36)
+            value.insert(0, "0");
+
+        for (int i = 0; i < mask.length(); i++) {
+            if (mask.charAt(i) == 'X') {
+                int s = toReturn.size();
+                for (int j = 0; j < s; j++) {
+                    String add = toReturn.remove(0);
+                    toReturn.add(add + "0");
+                    toReturn.add(add + "1");
+                }
+            } else if (mask.charAt(i) == '1') {
+                for (int a = 0; a < toReturn.size(); a++)
+                    toReturn.set(a, toReturn.get(a) + "1");
+            } else if (mask.charAt(i) == '0')
+                for (int a = 0; a < toReturn.size(); a++)
+                    toReturn.set(a, toReturn.get(a) + value.charAt(i));
+        }
+        return toReturn;
+    }
+}

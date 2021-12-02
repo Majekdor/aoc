51c01f3165c1fe1c6d0085e8a28000e8fc14c091
kevinbarnes <ksbarnes39@gmail.com>
2021-12-02 13:11:58 -0500

    Day Two

--- 2021/src/day_two.rs
+++ 2021/src/day_two.rs
@@ -3,17 +3,43 @@ use std::fs;
 pub fn run() {
     println!("---------\n Day Two\n---------");
 
-    let contents = fs::read_to_string("src/day_one.txt").expect("File not found!");
+    let contents = fs::read_to_string("src/day_two.txt").expect("File not found!");
     let lines:Vec<String> = contents.split("\n").map(|s| s.to_string()).collect();
 
     // Part One
-    let count = 0;
+    let mut horizontal = 0;
+    let mut depth = 0;
+    for line in &lines {
+        let parts:Vec<String> = line.split(" ").map(|s| s.to_string()).collect();
+        let command:String = parts[0].parse().unwrap();
+        match command.as_ref() {
+            "forward" => horizontal += parts[1].parse::<i32>().unwrap(),
+            "down" => depth += parts[1].parse::<i32>().unwrap(),
+            "up" => depth -= parts[1].parse::<i32>().unwrap(),
+            _ => println!("Invalid command!")
+        }
+    }
 
-
-    println!("Part One: {}", count);
+    println!("Part One: {}", horizontal * depth);
 
     // Part Two
+    horizontal = 0;
+    depth = 0;
+    let mut aim = 0;
+    for line in &lines {
+        let parts:Vec<String> = line.split(" ").map(|s| s.to_string()).collect();
+        let command:String = parts[0].parse().unwrap();
+        let num:i32 = parts[1].parse().unwrap();
+        match command.as_ref() {
+            "forward" => {
+                horizontal += num;
+                depth += aim * num;
+            },
+            "down" => aim += num,
+            "up" => aim -= num,
+            _ => println!("Invalid command!")
+        }
+    }
 
-
-    println!("Part Two: {}", count);
+    println!("Part Two: {}", horizontal * depth);
 }
